bed84ecccd4fbe81ea5ca5387c37c3e4719a2fb8
quangminh <thanbathai@gmail.com>
2018-09-04 21:32:18 +0700

    change host

--- config/keys.js
+++ config/keys.js
@@ -5,7 +5,7 @@ module.exports = {
     clientID: '5750225804-isbafg4dvm16b1fb6clsrupom7kn6ofv.apps.googleusercontent.com',
     clientSecret: 'HNH_rKewjuaVYuDDSLx2W10v',
   },
-  frontendHost: 'http://localhost:1337',
+  frontendHost: 'http://localhost:80',
   secret: '@iluminati@',
   session: {
     cookieKey: 'dakaplaza'
